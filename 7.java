6186840fdcad192523a4c7a54d389a14b3abe9fe
Vladislavix 58 <34206192+vladislavix@users.noreply.github.com>
2023-03-05 18:37:29 +0300

    added expression calculator

---
+++ 7.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class ExpressionCalculator {
+    public static double calculateExpression1(double x) {
+        return 3 * x + 5;
+    }
+
+    public static double calculateExpression2(double a, double b) {
+        return (a + b) / (a - b);
+    }
+
+    public static double calculateExpression3(double a, double x, double b) {
+        double ax = a * x;
+        return ax / b;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        ExpressionCalculator calculator = new ExpressionCalculator();
+        ExpressionCalculator calculator1 = new ExpressionCalculator();
+        ExpressionCalculator calculator2 = new ExpressionCalculator();
+
+        double x = in.nextDouble();
+        double y1 = calculator.calculateExpression1(x);
+        System.out.println("y1 = " + y1);
+
+        double a = 3.0;
+        double b = 2.0;
+        double y2 = calculator1.calculateExpression2(a, b);
+        System.out.println("y2 = " + y2);
+
+        double y3 = calculator2.calculateExpression3(a, x, b);
+        System.out.println("y3 = " + y3);
+
+        in.close();
+    }
+}
